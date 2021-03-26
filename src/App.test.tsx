98ce817affcc8ec2c9e12ebce9d87c7a680b3374
ych605 <ych605@gmail.com>
2021-03-27 02:53:17 +0800

    Update test for App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders react app', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appElement = getByText(/Generic Message Component Challenge/i);
+  expect(appElement).toBeInTheDocument();
 });
